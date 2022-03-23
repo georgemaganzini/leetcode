5953f6b54b7892131bdd9dac8e623c5809b09eda
george2 <involver7@gmail.com>
2022-03-23 12:27:33 -0700

    Add 1-04

--- 1-Sliding-Window/03_smallestSubarray.js
+++ 1-Sliding-Window/03_smallestSubarray.js
@@ -1,12 +1,17 @@
 const smallestSubarray = (arr, s) => {
+    // Validate input array, initialize smallest answer so far to array length if possible, otherwise set to -1 to indicate error
     let smallest = arr.reduce((acc, curr) => acc + curr) >= s ? arr.length : -1
 
     let sum = 0;
     let start = 0;
     let currLen = 0;
+
+    // interate over "end" as usual, summing as we go
     for (let windowEnd =0; windowEnd < arr.length; windowEnd++){
         sum += arr[windowEnd];
 
+        // use a while loop to count length, check if its less than smallest, subtract start
+        // repeat until no longer >= s
         while (sum >= s){
             currLen = windowEnd - start + 1
             smallest = currLen < smallest ? currLen : smallest;
--- 1-Sliding-Window/04_longestSubstring.js
+++ 1-Sliding-Window/04_longestSubstring.js
@@ -1,9 +1,33 @@
-const longestSubstring = () => {
-    
+const longestSubstringKDistinct = (string, k) => {
+    let windowStart = 0
+    let longest = 0;
+    let charFrequency = {};
 
+    // loop to try to extend range
+    for (let windowEnd = 0; windowEnd < string.length; windowEnd++){
+        const rightChar = string[windowEnd];
+        if(!(rightChar in charFrequency)){
+            charFrequency[rightChar] = 0;
+        }
+        charFrequency[rightChar] += 1;
+        // shrink the sliding window, until we are left with 'k distinct characters in
+        // the charFrequency
+        while (Object.keys(charFrequency).length > k){
+            const leftChar = string[windowStart];
+            charFrequency[leftChar] -= 1;
+            if (charFrequency[leftChar] === 0){
+                delete charFrequency[leftChar];
+            }
+            windowStart += 1; // shrink the window
+        }
+        // remember the max length so far
+        longest = Math.max(longest, windowEnd - windowStart + 1);
+    }
+
+    return longest;
 }
 
 let string = "araaci";
-let k = 2;
+let k = 3;
 
-console.log(longestSubstring(string, k));
+console.log(longestSubstringKDistinct(string, k));
