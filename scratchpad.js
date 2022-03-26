088550c868bb42d28abeeff6189787b2418a20d4
george2 <involver7@gmail.com>
2022-03-26 05:26:19 -0700

    Add permutation match

--- scratchpad.js
+++ scratchpad.js
@@ -1,31 +1,47 @@
-const longestSubarrayWithSub = (arr, k) => {
+const stringPermutation = (str, pattern) => {
     let windowStart = 0;
-    let maxLength =0;
-    let maxOnesCount = 0;
+    let matched = 0;
+    let charFrequency = {};
 
-    // Try to extend the range [windowStart, windowEnd]
-    for (let windowEnd = 0; windowEnd < arr.length; windowEnd++){
-        if (arr[windowEnd] === 1){
-            maxOnesCount += 1;
+    for (i = 0; i < pattern.length; i++){
+        const chr = pattern[i];
+        if(!(chr in charFrequency)) {
+            charFrequency[chr] = 0;
         }
-
-        // current window size is from windowStart to windowEnd, overall we have a maximum
-        // of 1s repeating a maximum of 'maxOnesCount' times, this means that we can have a
-        //  window with 'maxOnesCount' 1s and the remaining are 0s which should replace
-        // with 1s. Now, if the remaining 0s are more than 'k', it is the time to shrink
-        // the window as we are not allowed to replace more than 'k' Os.
-        if ((windowEnd - windowStart +1 - maxOnesCount) > k){
-            if (arr[windowStart] === 1){
-                maxOnesCount -= 1;
-            }
-            windowStart += 1;
-        }
-
-        maxLength = Math.max(maxLength, windowEnd - windowStart +1);
+        charFrequency[chr] += 1;
     }
-    return maxLength
+
+    // Our goal is to match all the characters from the 'charFrequency' with the current
+    // window try to extend the range [windowStart, windowEnd]
+
+    for (windowEnd = 0; windowEnd < str.length; windowEnd++){
+        const rightChar = str[windowEnd];
+        if (rightChar in charFrequency){
+            // Decrement the frequency of matched character
+            charFrequency[rightChar] -= 1;
+            if (charFrequency[rightChar]=== 0){
+                matched += 1;
+            }
+        }
+
+
+        if (matched === Object.keys(charFrequency).length) return true;
+
+        // Shrink the sliding window
+        if (windowEnd >= pattern.length + 1){
+            leftChar = str[windowStart];
+            windowStart += 1;
+            if (leftChar in charFrequency){
+                if (charFrequency[leftChar]===0){
+                    matched -= 1;
+                }
+                charFrequency[leftChar] += 1;
+            }
+        }
+    }
+    return false;
 }
 
-const arr = [0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1];
-k = 3;
-console.log(longestSubarrayWithSub(arr, k));
+const str = "oidbcaf";
+const pattern = "abc";
+console.log(stringPermutation(str, pattern));
