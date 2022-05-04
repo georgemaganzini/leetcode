21733fbb372abe3affbe2a59a31007e8dab3fa9c
george2 <involver7@gmail.com>
2022-05-04 13:20:01 -0700

    Add findDuplicate using Floyd's algorithm

--- Medium's/findDuplicate.js
+++ Medium's/findDuplicate.js
@@ -1,7 +1,22 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
 var findDuplicate = function(nums) {
+    let slow = 0;
+    let fast = 0;
+    while (true) {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
 
+        if (slow === fast) break;
+    }
+
+    let slow2 = 0;
+
+    while (slow !== slow2) {
+        slow = nums[slow];
+        slow2 = nums[slow2];
+    }
+
+    return slow
 };
+
+let nums = [1,3,4,2,2];
+console.log(findDuplicate(nums))
