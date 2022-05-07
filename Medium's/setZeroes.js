c5d278a7ef701c29d2d7fd65edc016c2eaef31ed
george2 <involver7@gmail.com>
2022-05-07 13:36:38 -0700

    Setting matrix zeroes

--- Medium's/setZeroes.js
+++ Medium's/setZeroes.js
@@ -1,3 +1,16 @@
-var setZeroes = function(matrix) {
+// A straightforward solution using O(mn) space is probably a bad idea.
+//      i.e. making a second matrix
+// A simple improvement uses O(m + n) space, but still not the best solution.
+//      i.e. storing which whole rows/columns to zero
+// Could you devise a constant space solution?
 
+var setZeroes = function(matrix) {
+    const rowLength = matrix.length;
+    const colLength = matrix[0].length;
+    let rows = new Array(rowLength).fill(1);
+    let columns = new Array(colLength).fill(1)
+
+
+
+    return matrix;
 };
