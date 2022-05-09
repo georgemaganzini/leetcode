357768342a48fafa13b3e1d8e1a98bef7df7dd26
george2 <involver7@gmail.com>
2022-05-09 10:20:57 -0700

    Complete setZeroes in matrix in constant space

--- Medium's/setZeroes.js
+++ Medium's/setZeroes.js
@@ -3,15 +3,48 @@
 // A simple improvement uses O(m + n) space, but still not the best solution.
 //      i.e. storing which whole rows/columns to zero
 // Could you devise a constant space solution?
+    // flag matrix[0][0-colLength] as null to indicate column needs to be zeroed
+    // flag matrix[1-roWlength][0] as null to indicate row need to be zeroed
+    // use O(1) extra space to indicate if 1st (0th) row needs to be zeroed
 
 var setZeroes = function(matrix) {
-    const rowLength = matrix.length;
-    const colLength = matrix[0].length;
-    let rows = new Array(rowLength).fill(1);
-    let columns = new Array(colLength).fill(1)
-    // flag matrix[0][0-colLength] as null to indicate column needs to be zeroed
-    // flag matrix[0-roWlength][0] as null to indicate row need to be zeroed
-    // how to interpret matrix[0][0] ?
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+    let firstRowIndicator = false;
+
+    for (let r =0; r < rows; r++){
+        for (let c = 0; c < cols; c++){
+            if (matrix[r][c] === 0) {
+                matrix[0][c] = 0
+                if (r > 0){
+                    matrix[r][0] = 0
+                } else {
+                    firstRowIndicator = true;
+                }
+            }
+        }
+    }
+
+    for (let r = 1; r < rows; r++){
+        for (let c = 1; c < cols; c++){
+            if (matrix[0][c] === 0 || matrix[r][0] === 0){
+                matrix[r][c] = 0
+            }
+        }
+    }
+
+    if (matrix[0][0] === 0){
+        for (let r = 0; r< rows; r++){
+            matrix[r][0] = 0
+        }
+    }
+
+    if (firstRowIndicator){
+        for (let c = 0; c < cols; c++){
+            matrix[0][c] = 0
+        }
+    }
+
 
     return matrix;
 };
