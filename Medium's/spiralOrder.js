7e4940808abba5a5a405e643aa2f38316df94c71
george2 <involver7@gmail.com>
2022-05-11 12:51:15 -0700

    add pseudocode for spiralOrder

--- Medium's/spiralOrder.js
+++ Medium's/spiralOrder.js
@@ -1,7 +1,10 @@
-/**
- * @param {number[][]} matrix
- * @return {number[]}
- */
 var spiralOrder = function(matrix) {
 
 };
+
+// [0][0] -> [0][n], [0][n] -> [m][n]
+// [m][n] -> [m][0], [m][0] -> [1][0]
+
+
+// every 4th direction change, start going one less than previous max length or width (m or n initially)
+// flag element as 0 after added to results array, end when 0 is encountered
