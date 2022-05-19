cdbc1ee19835edb4ce11f22f6befd68b1e058e46
george2 <involver7@gmail.com>
2022-05-19 15:30:52 -0700

    Complete spiralOrder

--- Medium's/spiralOrder.js
+++ Medium's/spiralOrder.js
@@ -4,22 +4,36 @@ var spiralOrder = function(matrix) {
     let right = matrix[0].length - 1;
     let top = 0;
     let bot = matrix.length - 1;
-    let goRight = true;
+    let direction = "right";
 
-    while ((left <= right) && (top <= bot)){
-        if (goRight){
+    while (left <= right && top <= bot){
+        if (direction==="right"){
             for (let i = left; i <=right; i++){
                 res.push(matrix[top][i]);
             }
             top++;
-            goRight = false;
+            direction = "down";
         }
-        if (!goRight){
+        else if (direction==="down"){
             for (let i = top; i <= bot; i++){
                 res.push(matrix[i][right])
             }
             right--;
-            goRight = true;
+            direction = "left";
+        }
+        else if (direction==="left"){
+            for (let i = right; i >= left; i--){
+                res.push(matrix[bot][i])
+            }
+            bot--;
+            direction = "up";
+        }
+        else if (direction==="up"){
+            for (let i = bot; i >= top; i--){
+                res.push(matrix[i][left])
+            }
+            left++;
+            direction = "right";
         }
     }
     return res;
