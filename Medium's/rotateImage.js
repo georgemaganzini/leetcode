f29666af9a3befefd06f7043d2ad5f87dbc6845c
george2 <involver7@gmail.com>
2022-05-24 09:32:31 -0700

    complete Rotate Image

--- Medium's/rotateImage.js
+++ Medium's/rotateImage.js
@@ -1,7 +1,28 @@
-/**
- * @param {number[][]} matrix
- * @return {void} Do not return anything, modify matrix in-place instead.
- */
 var rotate = function(matrix) {
+    let left = 0;
+    let right = matrix[0].length - 1;
 
+    while (left < right){
+        for (let i =0; i < right - left; i++){
+            let top = left;
+            let bottom = right;
+
+            // save top-left value
+            let topLeft = matrix[top][left + i];
+
+            // move bottom left into top left
+            matrix[top][left + i] = matrix[bottom - i][left];
+
+            // bottom right -> bottom left
+            matrix[bottom - i][left] = matrix[bottom][right - i];
+
+            // top right -> bottom right
+            matrix[bottom][right - i] = matrix[top + i][right];
+
+            // top left -> top right
+            matrix[top + i][right] = topLeft;
+        }
+        right--;
+        left++;
+    }
 };
