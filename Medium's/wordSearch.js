0768067cea270d14ce5baad446130b037d0262c8
george2 <involver7@gmail.com>
2022-05-31 17:42:57 -0700

    complete initial wordSearch tests

--- Medium's/wordSearch.js
+++ Medium's/wordSearch.js
@@ -1,20 +1,26 @@
 var exist = function(board, word) {
     let rows = board.length;
     let cols = board[0].length;
-    let path = new Set()
+    let path = new Set();
 
     const dfs = (r, c, i) => {
         if (i == word.length) return true;
-        if (r < 0 || c < 0 || r >= rows || c >= cols || word[i] != board[r][c] || (r,c) in path) return false;
+        if (r < 0 || c < 0 || r >= rows || c >= cols || word[i] != board[r][c] || [r,c] in path) return false;
 
-        path.add((r,c));
-        let res = (dfs(r + 1, c, i + 1)) ||
+        path.add([r,c]);
+        let res = ((dfs(r + 1, c, i + 1)) ||
                 (dfs(r - 1, c, i + 1)) ||
                 (dfs(r, c + 1, i + 1)) ||
-                (dfs(r, c - 1, i + 1));
-        path.remove((r,c));
+                (dfs(r, c - 1, i + 1)));
+        path.delete([r,c]);
+        // console.log(path)
         return res;
     }
 
-    for (let i = 0; i < )
+    for (let r = 0; r < rows; r++){
+        for (let c = 0; c < cols; c++) {
+            if (dfs(r, c, 0)) return true;
+        }
+    }
+    return false;
 };
