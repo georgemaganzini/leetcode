72aa08924a58078cb4921ddccf4b8870414d4e0f
george2 <involver7@gmail.com>
2022-06-02 19:44:38 -0700

    update letterCasePermutation

--- Medium's/letterCasePermutation.js
+++ Medium's/letterCasePermutation.js
@@ -1,10 +1,29 @@
 var letterCasePermutation = function(s) {
     let res = [];
 
-    const bfs = (string, index) => {
-        if (string[index].toLowerCase() != string[index].toUpperCase()) return string;
+    const dfs = (s, i, word) => {
+        if (i === s.length) {
+            res.push(word.join(''));
+            return;
+        }
 
-        //
+        let char = s[i];
+
+        if (!Number.isInteger(parseInt(char))){
+            word.push(char.toUpperCase());
+            dfs(s, i + 1, word);
+            word.pop();
+
+            word.push(char.toLowerCase());
+            dfs(s, i + 1, word);
+            word.pop();
+        } else {
+            word.push(char)
+            dfs(s, i + 1, word);
+        }
     }
 
+    dfs(s, 0, []);
+
+    return res;
 };
