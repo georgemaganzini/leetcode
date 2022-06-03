ed9de87bde34707337c3942a55fae24aef26cfbb
george2 <involver7@gmail.com>
2022-06-02 17:46:20 -0700

    start letterCasePermutation

--- Medium's/letterCasePermutation.js
+++ Medium's/letterCasePermutation.js
@@ -1,7 +1,10 @@
-/**
- * @param {string} s
- * @return {string[]}
- */
 var letterCasePermutation = function(s) {
+    let res = [];
+
+    const bfs = (string, index) => {
+        if (string[index].toLowerCase() != string[index].toUpperCase()) return string;
+
+        //
+    }
 
 };
