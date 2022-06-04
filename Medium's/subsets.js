3741dfb198bad8d75dd187cb0337286802faf019
george2 <involver7@gmail.com>
2022-06-04 14:48:58 -0700

    complete subsets I

--- Medium's/subsets.js
+++ Medium's/subsets.js
@@ -1,7 +1,27 @@
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
 var subsets = function(nums) {
+    let result = [];
 
+    const dfs = (i, nums, subset) => {
+
+        if (i === nums.length) {
+            // don't forget to make a copy aka .slice()
+            result.push(subset.slice());
+            return;
+        }
+        dfs(i + 1, nums, subset);
+
+
+        subset.push(nums[i]);
+        dfs(i + 1, nums, subset);
+        subset.pop(nums[i]);
+
+
+    }
+
+    dfs(0, nums, []);
+    return result;
 };
+
+
+let nums = [1,2,3];
+console.log(subsets(nums))
