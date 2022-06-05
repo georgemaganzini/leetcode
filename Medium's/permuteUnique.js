b76643e8a87bde5a04c7a185d9da2b444c8f89ce
george2 <involver7@gmail.com>
2022-06-05 10:19:38 -0700

    complete permuteUnique

--- Medium's/permuteUnique.js
+++ Medium's/permuteUnique.js
@@ -1,7 +1,25 @@
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
 var permuteUnique = function(nums) {
+    let result = [];
 
+    const dfs = (i, nums) => {
+        if (i === nums.length) {
+            result.push(nums.slice());
+            return;
+        }
+
+        let map = new Map();
+
+        for (let j = i; j < nums.length; j++){
+
+            if (map.has(nums[j])) continue;
+            map.set(nums[j], true);
+
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            dfs(i + 1, nums);
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+        }
+    }
+
+    dfs(0, nums);
+    return result;
 };
