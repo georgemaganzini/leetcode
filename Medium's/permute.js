cf1fdf99442af51689df1e1b68c0a418a15e83c7
george2 <involver7@gmail.com>
2022-06-04 20:28:10 -0700

    complete permute.js

--- Medium's/permute.js
+++ Medium's/permute.js
@@ -1,7 +1,22 @@
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
 var permute = function(nums) {
+    let result = [];
 
+    const dfs = (i, nums) => {
+        if (i === nums.length) {
+            result.push(nums.slice());
+            return;
+        }
+
+        for (let j = i; j < nums.length; j++){
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            dfs(i + 1, nums);
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+        }
+    }
+
+    dfs(0, nums);
+    return result;
 };
+
+let nums = [1,2,3];
+console.log(permute(nums));
--- Medium's/subsetsWithDup.js
+++ Medium's/subsetsWithDup.js
@@ -1,7 +1,31 @@
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
 var subsetsWithDup = function(nums) {
+    let result = [];
 
+    nums.sort((a, b) => a - b);
+
+    const dfs = (i, nums, subset) => {
+        if (i === nums.length){
+            result.push(subset.slice());
+            return;
+        }
+
+        dfs(i + 1, nums, subset);
+
+        subset.push(nums[i]);
+        dfs(i + 1, nums, subset);
+        subset.pop(nums[i]);
+
+    }
+
+    dfs(0, nums, []);
+    const hash = {};
+    for (let el of result){
+        if (hash[el]) continue;
+        hash[el] = el;
+    }
+    
+    return Object.values(hash);
 };
+
+let nums = [1,2,2];
+console.log(subsetsWithDup(nums))
