e876c087b5f464248535c51fc3ca8cb3efaff9cc
george2 <involver7@gmail.com>
2022-06-06 09:52:03 -0700

    add numSplits and minDifference

--- Medium's/combine.js
+++ Medium's/combine.js
@@ -1,8 +1,14 @@
-/**
- * @param {number} n
- * @param {number} k
- * @return {number[][]}
- */
 var combine = function(n, k) {
+    let result = [];
 
+    const dfs = () => {
+        
+    }
+
+    dfs();
+    return result;
 };
+
+let n = 4;
+let k = 2;
+console.log(combine(4,2));
