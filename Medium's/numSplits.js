665c3bf1f2b18a5fe2d62510ad4db1a8ecd873a1
george2 <involver7@gmail.com>
2022-06-06 16:21:33 -0700

    complete numSplits faster

--- Medium's/numSplits.js
+++ Medium's/numSplits.js
@@ -1,7 +1,19 @@
-/**
- * @param {string} s
- * @return {number}
- */
 var numSplits = function(s) {
-
+    let counter = 0;
+    let left = new Map();
+    let right = new Map();
+    for (let i = 0; i < s.length; i++){
+        right.set(s[i], (right.get(s[i]) || 0) + 1)
+    }
+    for (let i = 0; i < s.length; i++){
+        left.set(s[i], (left.get(s[i]) || 0) + 1)
+        let rightCurVal = right.get(s[i]) - 1;
+        right.set(s[i], rightCurVal)
+        if (rightCurVal === 0) right.delete(s[i])
+        if (left.size === right.size) counter++;
+    }
+    return counter;
 };
+
+let s = "aaaaa";
+console.log(numSplits(s))
