a42fb974c66434b81096d8e85f3c647e6d04e9d3
george2 <involver7@gmail.com>
2022-06-07 08:18:31 -0700

    complete combinationSum2

--- Medium's/combinationSum2.js
+++ Medium's/combinationSum2.js
@@ -1,3 +1,32 @@
 var combinationSum2 = function(candidates, target) {
+    let result = [];
 
+    candidates.sort((a, b) => a - b);
+
+    const dfs = (target, slate, i) => {
+        if (target < 0) return;
+
+        if (target === 0) {
+            result.push(slate.slice());
+            return;
+        }
+
+        for (let j = i; j < candidates.length; j++){
+            const curVal = candidates[j];
+
+            if (i !== j && curVal === candidates[j - 1]) continue;
+
+            slate.push(curVal);
+            dfs(target - curVal, slate, j + 1)
+            slate.pop();
+        }
+    }
+
+    dfs(target, [], 0);
+    return result;
 };
+
+let candidates = [10, 1, 2, 7, 6, 1, 5];
+let target = 8;
+
+console.log(combinationSum2(candidates, target))
