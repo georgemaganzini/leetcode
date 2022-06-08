25d2759cd8cece2f219100b162fddf7589dfd2f7
george2 <involver7@gmail.com>
2022-06-07 17:28:28 -0700

    add fib version of rob.js

--- Medium's/rob.js
+++ Medium's/rob.js
@@ -1,28 +1,58 @@
 var rob = function(nums) {
-    let max = 0;
+    /*
+    O(n) time, O(1) space
 
-    let topScore = [];
+	- Bottom up strategy
+	- Iterative
+	- Memoization
 
-    const dfs = (curr, i) => {
-        if (i > nums.length - 1) {
-            max = Math.max(curr, max);
-            return;
-        }
+	Trick: At index [i], you only need to know the maximum profit at [i - 1] and [i - 2]. This is a slight variation on fibonacci.
+    */
 
-        if (curr + nums[i] > topScore[i] || topScore[i] === undefined) {
-            topScore[i] = curr + nums[i];
-            dfs(curr + nums[i], i + 2)
-        }
+    if (!nums.length) return 0;
+    if (nums.length === 1) return nums[0];
+    if (nums.length === 2) return Math.max(nums[0], nums[1]);
 
-        dfs(curr, i + 1)
+    let maxAtTwoBefore = nums[0];
+    let maxAtOneBefore = Math.max(nums[0], nums[1]);
 
+    for (let i = 2; i < nums.length; i++) {
+        const maxAtCurrent = Math.max(nums[i] + maxAtTwoBefore, maxAtOneBefore);
 
+        maxAtTwoBefore = maxAtOneBefore;
+        maxAtOneBefore = maxAtCurrent;
     }
 
-    dfs(0, 0);
-    return max;
+    return maxAtOneBefore;
 };
 
-let nums = [1,2,3,1];
 
-console.log((rob(nums)));
+
+// var rob = function(nums) {
+//     let max = 0;
+
+//     let topScore = [];
+
+//     const dfs = (curr, i) => {
+//         if (i > nums.length - 1) {
+//             max = Math.max(curr, max);
+//             return;
+//         }
+
+//         if (curr + nums[i] > topScore[i] || topScore[i] === undefined) {
+//             topScore[i] = curr + nums[i];
+//             dfs(curr + nums[i], i + 2)
+//         }
+
+//         dfs(curr, i + 1)
+
+
+//     }
+
+//     dfs(0, 0);
+//     return max;
+// };
+
+// let nums = [1,2,3,1];
+
+// console.log((rob(nums)));
