6e7f3843dbc137cfcd8f25858ad547cf8ab93777
george2 <involver7@gmail.com>
2022-06-09 13:14:43 -0700

    clarify comments and variables

--- Medium's/longestPalindrome.js
+++ Medium's/longestPalindrome.js
@@ -4,6 +4,33 @@
 // edge cases around even vs odd length palindrome, sometimes around s.length of 0,1,2 (the latter relevant with this setup)
 
 var longestPalindrome = function(s) {
+    let max = '';
+    const findLongestPalindrome = (str, i, j) => {
+        while(i >= 0 && j < str.length && str[i] === str[j]) {
+            i--;
+            j++;
+        }
+        // slice the qualified substring from the second last iteration
+        return str.slice(i + 1, j);
+    }
+    for (let i = 0; i < s.length; i++) {
+        // palindrome can center around 1 or 2 letters
+        const odd = findLongestPalindrome(s, i, i);
+        const even = findLongestPalindrome(s, i, i + 1);
+        const longer = odd.length > even.length ? odd : even;
+
+        max = longer.length > max.length ? longer : max
+
+    }
+    return max;
+};
+
+let s = "ac";
+console.log(longestPalindrome(s));
+
+// my original,
+
+var longestPalindrome = function(s) {
     let max = "";
 
     const checkPalindromeLength = (curr, l, r) => {
@@ -25,31 +52,3 @@ var longestPalindrome = function(s) {
 
     return max;
 }
-
-let s = "ac";
-console.log(longestPalindrome(s));
-
-// fastest version:
-
-// var longestPalindrome = function(s) {
-//     let longest = '';
-//     const findLongestPalindrome = (str, i, j) => {
-//         while(i >= 0 && j < str.length && str[i] === str[j]) {
-//             i -= 1;
-//             j += 1;
-//         }
-//         // slice the qualified substring from the second last iteration
-//         return str.slice(i + 1, j);
-//     }
-//     for (let i = 0; i < s.length; i++) {
-//         // palindrome can center around 1 or 2 letters
-//         const current1 = findLongestPalindrome(s, i, i);
-//         const current2 = findLongestPalindrome(s, i, i + 1);
-//         const longerPalindrome =
-//               current1.length > current2.length ? current1 : current2;
-//         if (longerPalindrome.length > longest.length) {
-//             longest = longerPalindrome;
-//         }
-//     }
-//     return longest;
-// };
