3568c99197d565f698189433b09477fc41b38ea4
george2 <involver7@gmail.com>
2022-06-10 10:26:17 -0700

    add comments to wordBreak

--- Medium's/wordBreak.js
+++ Medium's/wordBreak.js
@@ -1,4 +1,10 @@
+// inner loop finds potential prefix(es)
+// outer loop starts at indexs after prefix is found
+// becareful with indicies of slice, initial table (length+1), and return (length - 1)
+
 var wordBreak = function(s, wordDict) {
+    // s.length + 1 is necessary because of how secondary paramater of slice (j in this case) is "up until aka non inclusive"
+    // i.e. table indicies represent if the substring ending before that index have been taken care of already
     const table = new Array(s.length + 1).fill(false);
 
     table[0] = true;
@@ -16,6 +22,6 @@ var wordBreak = function(s, wordDict) {
 };
 
 let s = "leetcode",
-    wordDict = ["leet","code"];
+    wordDict = ["leet", "lee", "code"];
 
 console.log(wordBreak(s, wordDict));
