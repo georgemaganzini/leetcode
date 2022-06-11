a5777f29096607801be4c8c641bfdb4d961343d6
george2 <involver7@gmail.com>
2022-06-11 14:00:02 -0700

    complete numDecodings

--- Medium's/numDecodings.js
+++ Medium's/numDecodings.js
@@ -1,28 +1,20 @@
 var numDecodings = function(s) {
-    if (s[0]=== 0) return 0;
 
-    let alpha = {1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "F", 7: "G", 8: "H",
-        9: "I", 10: "J", 11: "K", 12: "L", 13: "M", 14: "N", 15: "O", 16: "P", 17: "Q",
-        18: "R", 19: "S", 20: "T", 21: "U", 22: "V", 23: "W", 24: "X", 25: "Y", 26: "Z"
-    }
+    let dp = Array(s.length+1).fill(0);
 
-    let dp = Array(s.length).fill(1);
+    if (s[0]==='0') return 0
 
     dp[0] = 1;
+    dp[1] = 1;
 
-    let result = 0;
+    for (let i = 2; i <= s.length; i++){
+        const oneDigit = +s.slice(i-1, i);
+        const twoDigits = +s.slice(i-2, i);
 
-    const dfs = (i) {
-        if (i in dp) return dp[i]
-        if (s[i]==="0") return 0;
-
-        res = dfs(i + 1)
-        if ((i + 1 < s.length && s[i]==="1") ||
-            (s[i]==="2" && s[i+1]))
-
+        if (oneDigit > 0) dp[i] = dp[i-1];
+        if (twoDigits >= 10 && twoDigits <= 26) dp[i] += dp[i-2]
     }
-
-    return dp
+    return dp[s.length];
 };
 
 let s = "11106";
