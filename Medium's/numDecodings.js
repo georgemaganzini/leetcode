34651af6cc10389811f6845e468b570de5c1f2c1
george2 <involver7@gmail.com>
2022-06-12 15:45:35 -0700

    add comments and review uniquePaths and numDecodings

--- Medium's/numDecodings.js
+++ Medium's/numDecodings.js
@@ -8,11 +8,18 @@ var numDecodings = function(s) {
     dp[1] = 1;
 
     for (let i = 2; i <= s.length; i++){
+        // + being used as unary operator here, turns string into a number
         const oneDigit = +s.slice(i-1, i);
         const twoDigits = +s.slice(i-2, i);
 
+        // just make sure the number is valid (not zero)
+        // then continue keeping track of the paths that got to i-1 position if
+        // last single digit was a valid continuation of path
         if (oneDigit > 0) dp[i] = dp[i-1];
+        // make sure there is no leading zero, and also <= 26
+        // then add on the valid paths to two digits ago
         if (twoDigits >= 10 && twoDigits <= 26) dp[i] += dp[i-2]
+        // console.log(oneDigit, twoDigits, dp);
     }
     return dp[s.length];
 };
--- Medium's/uniquePaths.js
+++ Medium's/uniquePaths.js
@@ -1,9 +1,12 @@
 var uniquePaths = function(m, n) {
-    const table = Array.from({length: m}, () => new Array(n));
+    const table = Array(m).fill(Array(n));
 
-    for (let i = 0; i < table.length; i++) table[i][0] = 1;
-    for (let i = 0; i < table[0].length; i++) table[0][i] = 1;
+    // fill straight right and straight down paths with 1 possibility
+    for (let i = 0; i < m; i++) table[i][0] = 1;
+    for (let i = 0; i < n; i++) table[0][i] = 1;
 
+
+    // iterate over everything else and sum grid location north and west of current location
     for (let i = 1; i < table.length; i++) {
         for (let j = 1; j < table[i].length; j++){
             table[i][j] = table[i-1][j] + table[i][j-1];
