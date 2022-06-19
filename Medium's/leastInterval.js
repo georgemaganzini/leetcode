1f38aa6c8d0be20837530eb0a68e8d941b22e709
george2 <involver7@gmail.com>
2022-06-19 09:22:54 -0700

    complete leastInterval w/o heap

---
+++ Medium's/leastInterval.js
@@ -0,0 +1,17 @@
+const leastInterval = (tasks, n)=> {
+// create hashmap for frequencies of each task
+ const newMap = tasks.reduce((map, obj)=> {
+    map[obj] = map[obj] + 1 || 1 ;
+    return map;
+  }, {});
+
+// figure out which is the most frequent task
+const array=Object.values(newMap)
+// this is the max amount of "rows" we need
+const maximumRows = Math.max(...array)
+const lastRowLength = array.filter(x => x === maximumRows).length
+// final formula will be max rows - 1 (because we do not need to cooldown for last row)
+// we do still need to add the total amount of tasks in "last row" just omitting the final idle sequence
+// Math.max with tasks.length is for when n = 0;
+return Math.max(tasks.length, (maximumRows - 1) * (n + 1) + lastRowLength)
+};
