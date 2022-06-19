7fcc50e68e490b4c1738674f619fe9efa1de7e1e
george2 <involver7@gmail.com>
2022-06-19 15:53:39 -0700

    complete findClosestElements

---
+++ Medium's/findClosestElements.js
@@ -0,0 +1,18 @@
+// using binary search to determine window
+
+var findClosestElements = function(arr, k, x) {
+    let left = 0;
+    let right = arr.length - k;
+
+    while (left < right) {
+        let mid = Math.floor((left + right)/2)
+        if (x - arr[mid] > arr[mid + k] - x) left = mid + 1;
+        else right = mid; // no -1 here because tiebreaker is lower number i.e. rather have the window 1 to the left than psuedorandomily determined
+    }
+
+
+    return arr.slice(left, left + k);
+};
+
+let arr = [1,1,1,10,10,10], k = 1, x = 9;
+console.log(findClosestElements(arr, k, x));
