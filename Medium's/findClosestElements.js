bfcf3116c64f575813f528835ea8eef5c90f17fb
george2 <involver7@gmail.com>
2022-06-21 08:35:03 -0700

    review and add comments to findClosestElements and lengthOfLongestSubstring

--- Medium's/findClosestElements.js
+++ Medium's/findClosestElements.js
@@ -2,10 +2,10 @@
 
 var findClosestElements = function(arr, k, x) {
     let left = 0;
-    let right = arr.length - k;
+    let right = arr.length - k; // -k because we are looking for window start, and do not want window to go out of bounds
 
     while (left < right) {
-        let mid = Math.floor((left + right)/2)
+        let mid = Math.floor(left + (right - left)/2)
         if (x - arr[mid] > arr[mid + k] - x) left = mid + 1;
         else right = mid; // no -1 here because tiebreaker is lower number i.e. rather have the window 1 to the left than psuedorandomily determined
     }
--- Medium's/lengthOfLongestSubstring.js
+++ Medium's/lengthOfLongestSubstring.js
@@ -1,25 +1,29 @@
+// sliding window
+
 var lengthOfLongestSubstring = function(s) {
     let max = 0;
     let start = 0;
     const hash = {};
 
+    // iterate with end
     for (let end = 0; end < s.length; end++){
         let rightChar = s[end];
         hash[rightChar] = hash[rightChar] + 1 || 1;
 
+    // when duplicate letter is detected, start shrinking window from left until no value is > 1
         while (hash[rightChar] > 1){
             let leftChar = s[start];
 
             if (hash[leftChar] > 1){
-                hash[leftChar]--;
+                hash[leftChar]--;   // decrement if > 1
             } else {
-                delete hash[leftChar];
+                delete hash[leftChar]; // otherwise delete
             }
 
-            start++;
+            start++;    // increment left pointer to keep shrinking window until it is valid
         }
 
-        max = Math.max(max, (end - start) + 1);
+        max = Math.max(max, (end - start) + 1); // take math.max at each iteration after checking that no values exceed 1
     }
     return max;
 }
