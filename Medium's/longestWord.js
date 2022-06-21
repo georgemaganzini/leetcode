2641ce5f10080c5bd5526e5a88dc7b4e6198be17
george2 <involver7@gmail.com>
2022-06-21 09:32:22 -0700

    add trie version of longestWord

--- Medium's/longestWord.js
+++ Medium's/longestWord.js
@@ -16,3 +16,77 @@ var longestWord = function(words) {
 
 let words = ["m","mo","moc","moch","mocha","l","la","lat","latt","latte","c","ca","cat"]
 console.log(longestWord(words));
+
+
+var longestWord = function(words) {
+    if (!words.length) return ''
+    words.sort()
+    let set = new Set()
+    let ans = ''
+    for (let i = 0; i < words.length; i++) {
+        let prev = words[i].substring(0, words[i].length - 1)
+        if (words[i].length === 1 || set.has(prev)) {
+          ans = words[i].length > ans.length ? words[i] : ans
+          set.add(words[i])
+        }
+    }
+    return ans
+};
+
+// trie version:
+var longestWord = function(words) {
+    let trie = new Trie()
+    for (let i = 0; i < words.length; i++) {
+      trie.add(words[i])
+    }
+    let longestWord = ''
+    var dfs = function(node, s) {
+      let keys = Object.keys(node.storage)
+      for (let i = 0; i < keys.length; i++) {
+        if (node.storage[keys[i]].isWord) {
+          dfs(node.storage[keys[i]], s + keys[i])
+        }
+      }
+      if (s.length > longestWord.length) {
+        longestWord = s
+      } else if (s.length === longestWord.length) {
+        longestWord = getSmallest(s, longestWord)
+      }
+    }
+    dfs(trie.root, '')
+    return longestWord
+};
+
+var getSmallest = function(a, b) {
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] < b[i]) {
+            return a
+        } else if (b[i] < a[i]) {
+            return b
+        }
+    }
+    return a
+}
+
+class Node {
+    constructor() {
+        this.isWord = false
+        this.storage = {}
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new Node()
+    }
+    add(word) {
+        let curr = this.root
+        for (let i = 0; i < word.length; i++) {
+            if (!curr.storage[word[i]]) {
+                curr.storage[word[i]] = new Node()
+            }
+            curr = curr.storage[word[i]]
+        }
+        curr.isWord = true
+    }
+}
