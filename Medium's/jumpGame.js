fc53fd9272e61755ae2264522bb79f1c9e496b12
george2 <involver7@gmail.com>
2022-06-27 11:04:10 -0700

    complete jumpGame

--- Medium's/jumpGame.js
+++ Medium's/jumpGame.js
@@ -1,7 +1,10 @@
-/**
- * @param {number[]} nums
- * @return {boolean}
- */
 var canJump = function(nums) {
+    let goal = nums.length - 1;
 
+    for (let i = nums.length - 1; i >= 0; i--) if (i + nums[i] >= goal) goal = i;
+
+    return goal === 0 ? true : false;
 };
+
+let nums = [2,3,1,1,4];
+console.log(canJump(nums));
