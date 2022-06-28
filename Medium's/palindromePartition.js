ba66562c30a5874eae4041f05c573bc2af6f4b5e
george2 <involver7@gmail.com>
2022-06-28 10:14:26 -0700

    complete palindromePartition

--- Medium's/palindromePartition.js
+++ Medium's/palindromePartition.js
@@ -1,7 +1,36 @@
-/**
- * @param {string} s
- * @return {string[][]}
- */
-var partition = function(s) {
+const isPalindrome = (s, start, end) => {
+    while (start < end){
+        if (s[start] !== s[end]) return false;
+        start++;
+        end--;
+    }
 
+    return true;
+}
+
+var partition = function(s) {
+    let result = [];
+
+    const dfs = (i, s, slate) => {
+        // base case
+        if (i === s.length){
+            result.push(slate.slice());
+            return;
+        }
+
+        //dfs recursive case
+        for (let j = i; j < s.length; j++){
+            if (isPalindrome(s, i, j)){
+                slate.push(s.slice(i, j + 1));
+                dfs(j + 1, s, slate);
+                slate.pop();
+            }
+        }
+    }
+
+    dfs(0, s, []);
+    return result;
 };
+
+let s = "aab"
+console.log(partition(s))
