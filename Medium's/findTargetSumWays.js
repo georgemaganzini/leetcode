4998ee5dc24e45cbef20a2cc063456b45948e00a
george2 <involver7@gmail.com>
2022-06-29 11:42:51 -0700

    complete findTargetSumWays without dp

--- Medium's/findTargetSumWays.js
+++ Medium's/findTargetSumWays.js
@@ -1,8 +1,19 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
- */
 var findTargetSumWays = function(nums, target) {
+    let ways = 0;
 
+    const dfs = (i, curSum) => {
+        if (i === nums.length){
+            if (curSum === target) ways++;
+            return;
+        }
+
+        dfs(i + 1, curSum + nums[i]);
+        dfs(i + 1, curSum - nums[i]);
+    }
+
+    dfs(0, 0);
+    return ways;
 };
+
+let nums = [1,1,1,1,1], target = 3;
+console.log(findTargetSumWays(nums)); 
