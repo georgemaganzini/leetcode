184b302979c25348b00e8d64e8203c91d26bed68
george2 <involver7@gmail.com>
2022-07-06 12:43:53 -0700

    work on swapPairs

--- Medium's/swapPairs.js
+++ Medium's/swapPairs.js
@@ -1,14 +1,53 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-var swapPairs = function(head) {
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 
+  print_list() {
+    let temp = this;
+    while (temp !== null) {
+      process.stdout.write(`${temp.value} `);
+      temp = temp.next;
+    }
+    console.log();
+  }
+}
+
+
+var swapPairs = function(head) {
+    let secondNodeinPair = false;
+    let originalHead = head;
+    if (head.next) originalHead = head.next;
+
+    while (head) {
+
+        if (secondNodeinPair) {
+            // point prev node to next
+            prev.next = head.next
+            // point current node to prev
+            head.next = prev;
+            // hold new next in temporary variable
+            temp = prev.next
+            // adjust prev and head pointers
+            prev = head;
+            head = temp;
+        } else {
+            temp = head.next;
+            prev = head;
+            head = temp;
+        }
+
+        secondNodeinPair = !secondNodeinPair;
+    }
+
+    return originalHead ? originalHead : null;
 };
+
+
+const head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+
+console.log(swapPairs(head))
