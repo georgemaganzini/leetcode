23fbf0ebfcab35323e96b4689343b9cf878ec2e8
george2 <involver7@gmail.com>
2022-07-07 10:35:10 -0700

    add faster and clearer rotateList

--- Medium's/rotateList.js
+++ Medium's/rotateList.js
@@ -37,3 +37,27 @@ head.next = new ListNode(1);
 head.next.next = new ListNode(2);
 
 console.log(rotateRight(head, 4))
+
+// simple and clear with "fast and slow" model
+// start at dummy node rather than instantiate length at 1
+// careful with setting up for loop though (starts at 1, <= length - k)
+
+// var rotateRight = function(head, k) {
+//     let dummy = new ListNode()
+//     let slow = dummy
+//     let fast = dummy
+//     let length = 0
+//     dummy.next = head
+//     while(fast.next != null){
+//         fast = fast.next
+//         length++
+//     }
+//     k = k % length
+//     for (var i = 1; i <= length - k; i++) {
+//         slow = slow.next
+//     }
+//     fast.next = dummy.next
+//     dummy.next = slow.next
+//     slow.next = null
+//     return dummy.next
+// };
