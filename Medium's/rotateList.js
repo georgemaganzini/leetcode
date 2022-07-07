afcada9c008ee3b407afdca8679e27e57f5d7b98
george2 <involver7@gmail.com>
2022-07-07 10:28:34 -0700

    complete rotateList

--- Medium's/rotateList.js
+++ Medium's/rotateList.js
@@ -1,15 +1,39 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @param {number} k
- * @return {ListNode}
- */
-var rotateRight = function(head, k) {
 
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+var rotateRight = function(head, k) {
+    if (!head) return null;
+    let length = 1;
+    let originalHead = new ListNode(-Infinity, head);
+    originalHead = originalHead.next;
+
+    while (head.next) {
+        head = head.next;
+        length++;
+    }
+
+    head.next = originalHead;
+    head = originalHead;
+
+    // k can be larger than length, hence the mod
+    const walkDistance = length - k%length;
+
+    console.log(walkDistance, length)
+
+    for (let i = 0; i < walkDistance; i++) head = head.next;
+
+    let temp = head.next;
+    head.next = null;
+
+    return temp;
 };
+
+
+const head = new ListNode(0);
+head.next = new ListNode(1);
+head.next.next = new ListNode(2);
+
+console.log(rotateRight(head, 4))
