3956d867c1bb65fab134cf56b100b9cca0452a6f
george2 <involver7@gmail.com>
2022-07-08 08:56:45 -0700

    add pseudo for detectCycle

--- Medium's/linkedListCycleTwo.js
+++ Medium's/linkedListCycleTwo.js
@@ -11,5 +11,11 @@
  * @return {ListNode}
  */
 var detectCycle = function(head) {
+    // find cycle - fast and slow pointers, if they are ever equal, there is a cycle
+    // use a while loop with "!==null"
 
+    // find cycle length - from within cycle, store start node, then send a pointer forward counting how many nodes until current.val === start.val
+
+    // find cycle head - cheat pointer2 ahead cycleLength nodes,
+    // send both pointers forward at same speed, they will meet at start of cycle
 };
