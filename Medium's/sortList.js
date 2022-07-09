684cd2ae0e83c860a540ab13a3eff95f9b06527d
george2 <involver7@gmail.com>
2022-07-09 06:38:40 -0700

    complete sortList

--- Medium's/sortList.js
+++ Medium's/sortList.js
@@ -10,5 +10,46 @@
  * @return {ListNode}
  */
 var sortList = function(head) {
+    if (!head || !head.next) return head;
 
+    let left = head;
+    let right = getMid(head);
+    let temp = right.next;
+    right.next = null;
+    right = temp;
+
+    left = sortList(left);
+    right = sortList(right);
+
+    return merge(left, right);
 };
+
+const getMid = (head) => {
+    let slow = head;
+    let fast = head.next;
+    while (fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next
+    }
+    return slow;
+}
+
+const merge = (list1, list2) => {
+    let dummy = new ListNode();
+    let tail = dummy;
+
+    while (list1 && list2){
+        if (list1.val < list2.val){
+            tail.next = list1;
+            list1 = list1.next;
+        } else {
+            tail.next = list2;
+            list2 = list2.next;
+        }
+        tail = tail.next;
+    }
+    if (list1) tail.next = list1;
+    if (list2) tail.next = list2;
+
+    return dummy.next;
+}
