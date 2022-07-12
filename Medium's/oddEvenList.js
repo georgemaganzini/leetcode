78e3a5e8a815bca298cc76585f8ae1691fe51f8c
george2 <involver7@gmail.com>
2022-07-12 09:14:57 -0700

    complete oddEvenList with clearest version

--- Medium's/oddEvenList.js
+++ Medium's/oddEvenList.js
@@ -1,26 +1,38 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
 var oddEvenList = function(head) {
-    // adjust pointers to skip every other
-    // i.e. temp = node.next.next
-    // throw node.next to helper function (still pointing to temp but that's okay?)
-    // reassign node.next = temp;
-    // end when we hit tail (might need some logic depending on if tail falls on odd or even)
+    if (!head || !head.next) return head; // Watch out for edge cases! (0 or 1 item)
 
-    // helper function-> receives nodes, needs to connect them in order received then return head;
+    // Core strategy: make 2 chains (odds/evens) by "leapfrogging".
+    // No need to keep track of a counter or extra pointers, just need these 3:
+    let odd = head;
+    let even = head.next;
+    const evenHead = head.next;
 
+    // ("even.next" means more to go since even pointer is rightmost)
+    // ("even &&" is there to avoid null reference error, but also for even positions)
+    while (even && even.next) {
+        odd.next = even.next;
+        odd = odd.next;
+        even.next = odd.next;
+        even = even.next;
+    }
 
-    // connect tail to newHead
+    // Connect the two chains: (tail of odds to head of evens)
+    odd.next = evenHead;
 
+    return head;
+};
 
-    // return original head (or dummy.next)
-}; 
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+
+console.log(oddEvenList(head));
