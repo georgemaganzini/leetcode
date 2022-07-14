04ccc11a5549cfaf2e314d22594831270887cedb
george2 <involver7@gmail.com>
2022-07-13 19:18:44 -0700

    start kthSmallest

--- Medium's/kthSmallest.js
+++ Medium's/kthSmallest.js
@@ -4,5 +4,9 @@
  * @return {number}
  */
 var kthSmallest = function(matrix, k) {
-
+    if (k%matrix[0].length) {
+         skip = Math.floor(k / matrix[0].length);
+         remainder = k%matrix[0].length - 1;
+         return matrix[skip][remainder];
+    } else return matrix[k/matrix[0].length - 1][matrix[0].length - 1];
 };
