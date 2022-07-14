0708b5f8d75981f8f4f2ecab2008ef1a4e7bbe00
george2 <involver7@gmail.com>
2022-07-14 12:49:52 -0700

    complete eraseOverlapIntervals

---
+++ Medium's/eraseOverlapIntervals.js
@@ -0,0 +1,15 @@
+var eraseOverlapIntervals = function(intervals) {
+	// sort by earliest finish time
+    intervals.sort((a, b) => a[1] - b[1]);
+    let prev = intervals[0], remove = 0;
+
+    for(let i = 1; i < intervals.length; i++) {
+        if(intervals[i][0] < prev[1]) remove++;
+        else prev = intervals[i];
+    }
+    return remove;
+
+
+};
+let intervals = [[1,2],[2,3],[3,4],[1,3]];
+console.log(eraseOverlapIntervals(intervals));
