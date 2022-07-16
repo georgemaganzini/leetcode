6c65ac7005d7bc0bb7432e07d8faed8f08d5e69d
george2 <involver7@gmail.com>
2022-07-16 13:18:52 -0700

    complete searchTwo

--- Medium's/searchTwo.js
+++ Medium's/searchTwo.js
@@ -1,12 +1,23 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {boolean}
- */
 var search = function(nums, target) {
+    let l = 0;
+    let r = nums.length - 1;
 
+    while (l < r) {
+        mid = l + Math.floor((r - l) / 2);
+        if (nums[mid] === target) return true;
+
+        if (nums[l] < nums[mid]) {
+            if (nums[l] <= target && target < nums[mid]) r = mid - 1;
+            else l = mid + 1;
+        } else if (nums[mid] < nums[r]){
+            if (nums[mid] < target && target <= nums[r])l = mid + 1;
+            else r = mid - 1;
+        } else if (nums[r] === target) return true; // best we can do is just check one side and iterate it if other checks fail
+        else r--;
+    }
+    return (nums[l] === target);
 };
 
-let nums = [2,5,6,0,0,1,2], target = 0
+let nums = [3, 1], target = 0;
 
-console.log(search(nums, target)); 
+console.log(search(nums, target));
