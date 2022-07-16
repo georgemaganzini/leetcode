35c595dc4dab4cc5ba67c46c71a3110d13c09675
george2 <involver7@gmail.com>
2022-07-16 06:37:26 -0700

    add searchTwo

---
+++ Medium's/searchTwo.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function(nums, target) {
+
+};
+
+let nums = [2,5,6,0,0,1,2], target = 0
+
+console.log(search(nums, target));
