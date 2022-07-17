c0a132011afda9bd3fe74f58f843b866c81a62ec
george2 <involver7@gmail.com>
2022-07-17 12:57:04 -0700

    complete strStr with concise solution

---
+++ Easy's/removeElement.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let i = 0;
+    for (let j = 0; j < nums.length; j++){
+        if (nums[j] !== val) nums[i++] = nums[j];
+    }
+    return i;
+};
+
+let nums = [0,1,2,2,3,0,4,2], val = 2
+console.log(removeElement(nums, val));
---
+++ Easy's/strStr.js
@@ -0,0 +1,16 @@
+var strStr = function(haystack, needle) {
+    let indexOfStart = -1;
+
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle[0]){
+            for (let j = 0; j < needle.length; j++) {
+                if (needle[j] !== haystack[i + j]) break;
+                else if (j === needle.length - 1) return i;
+            }
+        }
+    return indexOfStart;
+};
+
+let haystack = "mississippi", needle = "issip";
+
+console.log(strStr(haystack, needle));
