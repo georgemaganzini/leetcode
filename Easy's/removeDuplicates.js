115c3b47390220b46197a959f349ccdd6f1e6661
george2 <involver7@gmail.com>
2022-07-17 11:06:53 -0700

    add notes to removeDuplicates

--- Easy's/removeDuplicates.js
+++ Easy's/removeDuplicates.js
@@ -1,3 +1,7 @@
+// i stores index position last non duplicate
+// when non duplicate is found after a string of duplicates, incremenent i and swap current with i (or place right after i just was)
+// return ++i because we want the length not the index
+
 var removeDuplicates = function(nums) {
     let i = 0;
     for (let j = 1; j < nums.length; j++) {
