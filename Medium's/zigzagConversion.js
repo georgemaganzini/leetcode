90cac07ba6acbb2aef72daf0185f1d46a0098411
george2 <involver7@gmail.com>
2022-07-20 09:47:24 -0700

    complete zigzag with notes

--- Medium's/zigzagConversion.js
+++ Medium's/zigzagConversion.js
@@ -3,24 +3,16 @@ var convert = function(s, numRows) {
     let result = Array(numRows).fill('');
 
     while (s.length > 0){
-    // down
+        // down
         for (i = 0; i < numRows; i++) {
-            if (s[0]) result[i] += s.shift();
+            if (s[0]) result[i] += s.shift(); // would use queue/deque here
         }
-
-        // console.log(s.length)
 
         // up, right
         for (i = numRows - 2; i > 0; i--) {
              if (s[0]) result[i] += s.shift();
         }
     }
-
-    console.log(result)
-
-    // for (let subArr of result){
-    //     resultString += ...subArr;
-    // }
 
     return result.join("");
 };
@@ -32,4 +24,5 @@ let s = "PAYPALISHIRING", numRows = 3;
 // P   A   H   N
 // A P L S I I G
 // Y   I   R
+
 console.log(convert(s, numRows))
