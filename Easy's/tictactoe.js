2f481d23cf64463d092ecd2568f43c237711eb3f
george2 <involver7@gmail.com>
2022-07-25 06:10:19 -0700

    refactor tictactoe

--- Easy's/tictactoe.js
+++ Easy's/tictactoe.js
@@ -1,18 +1,27 @@
-const tictactoe = moves => {
-    const cases = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-    const grid = new Uint8Array(9);
-    for (let i = 0; i < moves.length; ++i) {
-        grid[moves[i][0] * 3 + moves[i][1]] = (i % 2) + 1;
+var tictactoe = function(moves) {
+   const arr = new Array(9).fill(null)
+   const winCases = [
+       [0, 1, 2],
+       [3, 4, 5],
+       [6, 7, 8],
+       [0, 3, 6],
+       [1, 4, 7],
+       [2, 5, 8],
+       [0, 4, 8],
+       [2, 4, 6],
+   ]
+   const len = moves.length
+   for (let i = 0; i < len; i++) {
+       const move = moves[i]
+       arr[move[0] * 3 + move[1]] = i % 2 ? "B" : "A"
+   }
+    for (let win of winCases) {
+        const user = arr[win[0]]
+        if (user && user === arr[win[1]] && user === arr[win[2]]) return user
     }
-    for (let i = 0; i < cases.length; ++i) {
-        const role = grid[cases[i][0]];
-        if (role !== 0 && grid[cases[i][1]] === role && grid[cases[i][2]] === role) {
-        return role === 1 ? 'A' : 'B';
-        }
-    }
-    return moves.length === 9 ? 'Draw' : 'Pending';
+   return (len === 9) ? "Draw" : "Pending"
 };
 
-let moves = [[0,0],[2,0],[1,1],[2,1],[2,2]]; // Output: "A"
+let moves = [[0,0],[1,1],[0,1],[0,2],[1,0],[2,0]]; // Output: "B"
 
 console.log(tictactoe(moves));
