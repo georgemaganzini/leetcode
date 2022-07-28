fc2a4b3361b4aa3d82acc5ecaba89b6566823744
george2 <involver7@gmail.com>
2022-07-28 07:52:13 -0700

    complete addDigits with mod 9 arithmetic

---
+++ Easy's/addDigits.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+    return 1 + (num - 1) % 9;
+};
+
+let num = 388; // 2
+
+console.log(addDigits(num))
+
+// alternate format
+
+var addDigits = function(num) {
+    if (isNaN(num) || num === 0) return 0;
+    if (num < 10) return num;
+    return num % 9 === 0 ? 9 : num % 9;
+};
