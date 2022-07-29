f124a5481e43033932eab5113bfcd4de3545544f
george2 <involver7@gmail.com>
2022-07-29 15:05:37 -0700

    small adjust

--- Medium's/trie.js
+++ Medium's/trie.js
@@ -38,41 +38,50 @@ class Trie {
         }
         return true; //find the prefix word
     }
+
+    traverse(word) {
+        let node = this.root;
+        for (let c of word) {
+            node = node[c];
+            if (node == null) return null;
+        }
+        return node;
+    }
 }
 
 
 
 // w/ traverse:
 
-class Trie {
-  constructor() {
-    this.root = {};
-  }
+// class Trie {
+//   constructor() {
+//     this.root = {};
+//   }
 
-  insert(word) {
-    let node = this.root;
-    for (let c of word) {
-      if (node[c] == null) node[c] = {};
-      node = node[c];
-    }
-    node.isWord = true;
-  }
+//   insert(word) {
+//     let node = this.root;
+//     for (let c of word) {
+//       if (node[c] == null) node[c] = {};
+//       node = node[c];
+//     }
+//     node.isWord = true;
+//   }
 
-  traverse(word) {
-    let node = this.root;
-    for (let c of word) {
-      node = node[c];
-      if (node == null) return null;
-    }
-    return node;
-  }
+//   traverse(word) {
+//     let node = this.root;
+//     for (let c of word) {
+//       node = node[c];
+//       if (node == null) return null;
+//     }
+//     return node;
+//   }
 
-  search(word) {
-    const node = this.traverse(word);
-    return node != null && node.isWord === true;
-  }
+//   search(word) {
+//     const node = this.traverse(word);
+//     return node != null && node.isWord === true;
+//   }
 
-  startsWith(prefix) {
-    return this.traverse(prefix) != null;
-  }
-}
+//   startsWith(prefix) {
+//     return this.traverse(prefix) != null;
+//   }
+// }
